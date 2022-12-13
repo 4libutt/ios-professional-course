447aaceb31c02523d40166103597da3f7940eaea
4libutt <ali-butt@live.nl>
2022-12-13 11:23:51 +0100

    feature: dynamic truncation summary cell text

--- Bankey/Bankey/AccountSummary/Cells/AccountSummaryCell.swift
+++ Bankey/Bankey/AccountSummary/Cells/AccountSummaryCell.swift
@@ -73,6 +73,7 @@ extension AccountSummaryCell {
         
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        nameLabel.adjustsFontSizeToFitWidth = true
         nameLabel.text = "Account name"
         
         balanceStackView.translatesAutoresizingMaskIntoConstraints = false
@@ -81,6 +82,7 @@ extension AccountSummaryCell {
         
         balanceLabel.translatesAutoresizingMaskIntoConstraints = false
         balanceLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        balanceLabel.adjustsFontSizeToFitWidth = true 
         balanceLabel.textAlignment = .right
         balanceLabel.text = "Some balance"
         
---
+++ Bankey/Bankey/Utils/UITextField+SecureToggle.swift
@@ -0,0 +1,26 @@
+//
+//  UITextField+SecureToggle.swift
+//  Bankey
+//
+//  Created by Ali Butt on 13/12/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    func enablePasswordToggle() {
+        let passwordToggleButton = UIButton(type: .custom)
+        passwordToggleButton.tintColor = .systemBlue
+        passwordToggleButton.setImage(UIImage(named: "eye.fill"), for: .normal)
+        passwordToggleButton.setImage(UIImage(named: "eye.slash.fill"), for: .selected)
+        passwordToggleButton.addTarget(self, action: #selector(togglePasswordView), for: .touchUpInside)
+        rightView = passwordToggleButton
+        rightViewMode = .always
+    }
+    
+    @objc func togglePasswordView(_ sender: UIButton) {
+        isSecureTextEntry.toggle()
+        sender.isSelected.toggle()
+    }
+}
